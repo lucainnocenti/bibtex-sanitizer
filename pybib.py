30eb52395ef5aa4f18a897026f5c384e7ef89c24
Luca Innocenti <lukeinnocenti@gmail.com>
2019-10-24 20:52:58 +0100

    implement extract doi for nature shit

--- pybib.py
+++ pybib.py
@@ -33,16 +33,47 @@ class Re:
         self.last_match = re.search(pattern, text, *args)
         return self.last_match    
 
+
+def extract_doi_from_journal_url(url):
+    """Figure out journal from url and extract doi.
+
+    Note that here `url` has already been stripped of the https://www bit.
+    """
+    print(url[:10])
+    if url[:10] == 'nature.com':
+        # nature journals all have dois of the form 10.1038/X, with the X bit
+        # included in the title
+        basedoi = '10.1038'
+        gre = Re()
+        last_doi_bit = gre.match(r'nature\.com/articles/(.*)', url).group(1)
+        return basedoi + '/' + last_doi_bit
+    else:
+        raise ValueError('Not a bloody nature is it??')
+        
+
+
 def _extract_doi_from_url(string):
     gre = Re()
-    # strip initial url part, if present
+    # if the url provided contains directly a DOI, we just extract and use
+    # that one
     if gre.match(r'.*(?:doi\.org/)(.*)', string):
         return gre.last_match.group(1)
+    # some journals, most notably the APS ones, contain the DOI in the url,
+    # but the URL doesn't explicitly contain the word "DOI". If the url is
+    # found to contain a string of the form 10.1111/XXXXX, which resembles a
+    # DOI, we assume that this is the DOI we are looking for (hopefully this
+    # doesn't backfire!)
     elif gre.match(r'.*?([0-9]{2}\.[0-9]{4,5}.*)', string):
         # e.g. revmodphys has these kinds of urls
         return gre.last_match.group(1)
     else:
-        return string
+        # If the easy ways didn't work, we have to use ad-hoc methods tailored
+        # to each journal
+
+        # extract base url
+        gre = Re()
+        baseurl = gre.match(r'https?:\/\/(?:www\.)?(.*)', string).group(1)
+        return extract_doi_from_journal_url(baseurl)
 
 
 def _extract_arxiv_id_from_url(string):
@@ -86,7 +117,7 @@ def _print_reference(from_where, identifiers):
         output = bibtexsanitizer.get_bibentry_from_arxiv_id(ids)
     elif from_where == 'doi':
         ids = [_extract_doi_from_url(id_) for id_ in identifiers]
-        logger.info('I found the following dois in the given urls: {}'.format(ids))
+        logger.info('I extracted the following dois in the given urls: {}'.format(ids))
         output = bibtexsanitizer.get_bibentry_from_doi(ids)
     else:
         raise NotImplementedError('To Be Done.')
